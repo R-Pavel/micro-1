b532bc0f63230f109450461cdd28c578708d64d0
R-Pavel <prylsky@gmail.com>
2022-03-31 12:42:10 +0700

    crteate micriservice structure

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,16 @@
-// app.controller.ts
-
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
-
   @Get('/item/:id')
   getById(@Param('id') id: number) {
     return this.appService.getItemById(id);
   }
-
   @Post('/create')
   create(@Body() createItemDto) {
     return this.appService.createItem(createItemDto);
